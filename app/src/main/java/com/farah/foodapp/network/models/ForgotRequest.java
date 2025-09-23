1896840ad7f0ee0e4574a17164e63c12dc2c8834
FarahAmmourah <farahamorah17@gmail.com>
2025-09-23 21:54:40 +0300

    updated

--- app/src/main/java/com/farah/foodapp/network/models/ForgotRequest.java
+++
@@ -1,9 +0,0 @@
-//package com.farah.foodapp.network.models;
-//
-//public class ForgotRequest {
-//    public String email;
-//
-//    public ForgotRequest(String email) {
-//        this.email = email;
-//    }
-//}
--- app/src/main/java/com/farah/foodapp/network/models/UserResponse.java
+++
@@ -1,8 +0,0 @@
-//package com.farah.foodapp.network.models;
-//
-//public class UserResponse {
-//    public int id;
-//    public String email;
-//    public String username;
-//    public String name;
-//}
